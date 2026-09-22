b1fae2c97bd8a57284a10512db7f1f9dd80ca008
Nacho <nachogarciarivas@gmail.com>
2026-09-22 10:14:15 -0600

    Actualiza carpeta automatica de carpeta

---
+++ Actualizar.bat
@@ -0,0 +1,3 @@
+echo off
+cd C:\Users\Usuario\Documents\NetBeansProjects\3er_Cuatri_Progra_Cliente_Servidor
+git pull
---
+++ Commit Cambios Carpeta.bat
@@ -0,0 +1,5 @@
+echo off
+cd C:\Users\Usuario\Documents\NetBeansProjects\3er_Cuatri_Progra_Cliente_Servidor
+git add .
+git commit -m "Actualiza carpeta automatica de carpeta"
+git push
