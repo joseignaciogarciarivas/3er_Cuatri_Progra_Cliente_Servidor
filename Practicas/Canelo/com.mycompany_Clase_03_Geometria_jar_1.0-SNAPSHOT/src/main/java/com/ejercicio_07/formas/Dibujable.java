6387e1849747ba6eb7bd23bfd4eb6e0642291fdb
Andrés González García <andresgonzalez1988@hotmail.com>
2026-09-23 18:36:46 -0600

    Reacomodo de carpetas

--- Practicas/Canelo/com.mycompany_Clase_03_Geometria_jar_1.0-SNAPSHOT/src/main/java/com/ejercicio_07/formas/Dibujable.java
+++
@@ -1,7 +0,0 @@
-package com.ejercicio_07.formas;
-
-public interface Dibujable {
-
-    void dibujar();
-
-}
--- Practicas/Canelo/com.mycompany_Clase_03_Geometria_jar_1.0-SNAPSHOT/src/main/java/com/ejercicio_07/formas/Forma.java
+++
@@ -1,9 +0,0 @@
-package com.ejercicio_07.formas;
-
-public abstract class Forma {
-
-    public abstract double getArea();
-
-    public abstract double getPerimetro();
-
-}
